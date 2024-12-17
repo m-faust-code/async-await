3a58e57810602abc3be43f7cf712a332f1635034
Maggie Faust <mfaust@macalester.edu>
2024-12-17 17:58:56 -0600

    Started swift code

---
+++ swift-async-await/Sources/main.swift
@@ -0,0 +1,17 @@
+
+func asyncs() async {
+    print("hello")
+    do {
+        try await Task.sleep(nanoseconds:1000000000)
+    } catch {
+        print("error")
+    }
+    print("world") 
+}
+
+
+
+await asyncs()
+
+
+
