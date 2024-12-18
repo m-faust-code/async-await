837b67b4cc4a96fa86071671582d6379e3e12bc5
Maggie Faust <mfaust@macalester.edu>
2024-12-18 15:00:53 -0600

    Added tasks
    Why does Swift sleep for so long?

--- python-async-await.py
+++ python-async-await.py
@@ -1,7 +1,27 @@
 import asyncio
+import time
 
 async def asyncs():
     print('hello')
     await asyncio.sleep(1)
     print ('world')
 
+async def say_after(delay, what):
+    await asyncio.sleep(delay)
+    print(what)
+
+async def tasks():
+    async with asyncio.TaskGroup() as tg:
+        task1 = tg.create_task(
+            say_after(1, 'hello')
+        )
+
+        task2 = tg.create_task(
+            say_after(2, 'world')
+        )
+
+        print(f"started at {time.strftime('%X')}")
+    
+    print(f"finished at {time.strftime('%X')}")
+
+asyncio.run(tasks())
--- swift-async-await/Sources/main.swift
+++ swift-async-await/Sources/main.swift
@@ -1,16 +1,38 @@
 
+
+
 func asyncs() async {
     print("hello")
     do {
-        try await Task.sleep(nanoseconds:1000000000)
+        try await Task.sleep(for: .seconds(1))
     } catch {
         print("error")
     }
     print("world") 
 }
 
+func say_after(delay: Double, what: String) async-> Void{
+    do {
+        try await Task.sleep(for: .seconds(delay))
+    } catch {
+        print("error")
+    }
+    print(what)
+}
 
-await asyncs()
+func tasks() async {
+    let clock = ContinuousClock()
+    async let task1: Void = say_after(delay: 1, what: "hello")
+    async let task2: Void = say_after(delay: 2, what: "world")
+    let start_time = clock.now
+    let _ = await [task1, task2]
+    let end_time = clock.now
+    let total_time = start_time.duration(to: end_time)
+    print("total time:" + String(reflecting:total_time))
+}
+
+
+await tasks()
 
 
 
