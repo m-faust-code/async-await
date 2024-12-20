447d666f7a82ed5c152b910c00065b835dbe39e2
Maggie Faust <mfaust@macalester.edu>
2024-12-19 21:59:11 -0600

    Added actors

--- swift-async-await/Sources/main.swift
+++ swift-async-await/Sources/main.swift
@@ -31,7 +31,44 @@ func tasks() async {
     print("total time:" + String(reflecting:total_time))
 }
 
-await tasks()
+actor Duck {
+    var name: String
+
+    init(name: String) {
+        self.name = name
+    }
+
+    func quack() {
+        print(name + " quacks")
+    }
+
+    func quackAt(duck: Duck) async {
+        let other_name = await duck.name
+        print(name + " quacks at " + other_name)
+    }
+
+    func rename(to new_name: String) {
+        self.name = new_name
+    }
+}
+
+func actors() async {
+    let alice = Duck(name: "Alice")
+    let bob = Duck(name: "Bob")
+
+
+    async let task1: Void = alice.quack()
+    async let task2: Void = bob.quack()
+    async let task3: Void = alice.quackAt(duck: bob)
+    async let task4: Void = bob.quackAt(duck: alice)
+    async let task5: Void = alice.rename(to: "Alice 2")
+    async let task6: Void = bob.rename(to: "Bob 2")
+
+    await _ = [task1, task2, task3, task4, task5, task6]
+}
+
+
+await actors()
 
 
 
